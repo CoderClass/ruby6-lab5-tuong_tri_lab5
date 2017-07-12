884d3dc4d7aedabba67ab04192a698003f872afd
nvtuong <nvtuongcs@gmail.com>
2017-07-12 20:20:55 +0700

    Milestone 2: support submitting new chat messages.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,24 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def new
+  	@message = Message.new
+  end
+
+  def create
+  	@message = Message.new(message_params)
+  	if @message.save
+  		redirect_to messages_path
+  	else
+  		flash[:error] = "Erros: #{@message.errors.full_messages.to_sentence}"
+  		render 'new'
+  	end
+  end
+
+  private
+  def message_params
+  	params.require(:message).permit(:body)
+  end
+
 end
