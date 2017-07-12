288e018be3885923e5d4360716e1d98bb939c070
Tri Ngo <ngominhtrint@gmail.com>
2017-07-12 20:42:27 +0700

    Milestone 3: set up client-server WebSocket communication.

---
+++ app/channels/messages_channel.rb
@@ -0,0 +1,16 @@
+class MessagesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  # note it's 'receive', not 'received'
+  def receive(data)
+    Rails.logger.info("MessagesChannel got: #{data.inspect}")
+    ActionCable.server.broadcast("chat", data)
+  end
+
+end
